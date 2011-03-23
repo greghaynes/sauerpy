962ec5157e4db733c486e1f7dcb1806be365f369
Gregory Haynes <greg@greghaynes.net>
2011-03-23 02:32:50 -0700

    Added skeleton sauerserver.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import beaconserver
+import sauerserver
 import settings
 
 import asyncore
 
 if __name__ == "__main__":
 	beaconServer = beaconserver.BeaconServer(settings.hostname, settings.port)
+	sauerServer = sauerserver.SauerServer(settings.hostname, settings.port+1)
 	
 	asyncore.loop()
 
