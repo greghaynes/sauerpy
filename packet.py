c06277904ce42056a69f42a357df4bd615d571c1
Gregory Haynes <greg@greghaynes.net>
2011-03-23 03:42:29 -0700

    Added packet.pushint

--- packet.py
+++ packet.py
@@ -9,8 +9,8 @@ class Packet(object):
 			n, nn = c.struct.unpack_from('bb', self.raw_data)
 			return (n | (nn<<8))
 		else if c == -127:
-			n, nn, nnn = c.struct.unpack_from('bbb', self.raw_data)
-			return (n | (nn<<8) | (nnn<<16))
+			n, nn, nnn, nnnn = c.struct.unpack_from('bbb', self.raw_data)
+			return (n | (nn<<8) | (nnn<<16) | (nnnn<<24)
 		else
 			return c
 	def popInt(self):
@@ -18,8 +18,19 @@ class Packet(object):
 		if ret < 128 and ret > -127:
 			self.raw_data = self.raw_data[1:]
 		else if ret < 0x8000 and ret >= -0x8000:
-			self.raw_data = self.raw_data[4:]
+			self.raw_data = self.raw_data[3:]
 		else:
 			self.raw_data = self.raw_data[5:]
 		return ret
+	def pushInt(self, val):
+		if val > 128 and val < -127:
+			raw_data += struct.pack('b', val)
+		else if val < 0x8000 and val >= 0x8000:
+			raw_data += struct.pack('bbb', 0x80, val&0x0F, val>>8)
+		else:
+			raw_data += struct.pack('bbbbb', 0x81,
+			                        val&0xFF,
+			                        (val>>8)&0xFF,
+			                        (val>>16)&0xFF,
+			                        (val>>24)&0xFF)
 
