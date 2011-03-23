3a420e4a7e0fd3e36891f1eb791f2ae5249a603b
Gregory Haynes <greg@greghaynes.net>
2011-03-23 03:50:25 -0700

    Added advanced settings

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncore
 
 if __name__ == "__main__":
 	beaconServer = beaconserver.BeaconServer(settings.hostname, settings.port)
-	sauerServer = sauerserver.SauerServer(settings.hostname, settings.port+1)
+	sauerServer = sauerserver.SauerServer(settings.hostname, settings.sauerserver_port)
 	
 	asyncore.loop()
 
--- settings.py
+++ settings.py
@@ -4,3 +4,10 @@ hostname = ''
 # Server port
 port = 1337
 
+# *****************************************************************
+# Dont touch settings below here unless you know what youre doing |
+# *****************************************************************
+sauerserver_port = port + 1
+
+protocol_version = 257
+
