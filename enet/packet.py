7ac093095bc4dc3596ee96722ddb2d25fffd6ce3
Gregory Haynes <greg@greghaynes.net>
2011-04-08 02:22:07 -0700

    Added command header to packet __str__

--- enet/packet.py
+++ enet/packet.py
@@ -52,5 +52,10 @@ class EnetPacket(object):
 		ret += '\tcompressed: %d\n' % self.is_compressed
 		ret += '\tpeer id: %d\n' % self.peer_id
 		ret += '\tsession id: %d\n' % self.session_id
+		ret += '\tcommand: %d\n' % self.command
+		ret += 'Command Header:\n'
+		ret += '\toutgoing peer id: %d\n' % self.outgoing_peer_id
+		ret += '\tincoming session id: %d\n' % self.incoming_sess_id
+		ret += '\toutgoing session id: %d\n' % self.outgoing_sess_id
 		return ret
 
